862d00bc48ee9b2de3594620d1064610b0f59511
Tumpa <bsse1307@iit.du.ac.bd>
2025-03-13 12:53:06 +0600

    cookies practice, but getting cors error

---
+++ Cookies/practice_codes/samesite/server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+
+app.post("/", (req, res) => {
+    const cookie = req.headers.cookie;
+    if(cookie){
+        res.sendFile(`${__dirname}/cookie.png`);
+    }
+    else{
+        res.sendStatus(403);
+        res.end();
+    }
+})
+
+app.post("/login", (req, res) => {
+    const cookie = "user=tumpa; samesite=strict;";
+    // const cookie = "user=tumpa; samesite=lax; secure";
+    // const cookie = "user=tumpa; samesite=none; secure";
+    // const cookie = "user=tumpa;";
+
+    res.setHeader("set-cookie", [cookie]);
+    res.send("ok");
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+})
+
+app.get("/img", (req, res) => {
+    const cookie = req.headers.cookie;
+    if(cookie){
+        res.sendFile(`${__dirname}/cookie.png`);
+    }
+    else{
+        res.sendStatus(403); // forbidden
+        res.end();
+    }
+})
+
+app.listen(8080, ()=>{
+    console.log("server is listening to port 8080");
+    
+})
