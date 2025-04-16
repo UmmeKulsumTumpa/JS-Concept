e4c2f8731dd35235e0fba19c5ffe700327900735
Tumpa <bsse1307@iit.du.ac.bd>
2025-04-16 17:17:10 +0600

    typeof operator

---
+++ data-types/typeof-operator.js
@@ -0,0 +1,6 @@
+console.log(typeof null); // object
+
+console.log(typeof NaN); // number
+console.log(typeof Number("string will be converted to number")); // number
+
+console.log(typeof (typeof 1)); // string; as the typeof operator returns a string
